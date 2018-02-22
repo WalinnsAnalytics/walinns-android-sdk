775756db469feba3207d71ef1b44ffd48f58c5de
Walinns <rejoylin2015@gmail.com>
2018-02-22 09:22:50 +0530

    initial commit

--- StarterProject/app/src/main/java/walinns/com/starterproject/MainActivity.java
+++ StarterProject/app/src/main/java/walinns/com/starterproject/MainActivity.java
@@ -3,11 +3,16 @@ package walinns.com.starterproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.walinns.walinnsapi.WalinnsAPI;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WalinnsAPI.getInstance().initialize(MainActivity.this,"PROJECT_TOKEN");
+
     }
 }
